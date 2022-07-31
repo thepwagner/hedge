dd56d26f760513bf651a44f57add385876cc6b5d
Peter Wagner <1559510+thepwagner@users.noreply.github.com>
2022-07-30 22:08:54 -0400

    turf that

--- debian/control.go
+++ debian/control.go
@@ -123,20 +123,3 @@ var multilineKeys = map[string]struct{}{
 	"MD5Sum": {},
 	"SHA256": {},
 }
-
-// TODO: probably turf this...
-type kv struct {
-	k, v string
-}
-
-func writeKV(w io.Writer, entries ...kv) error {
-	for _, e := range entries {
-		if e.v == "" {
-			continue
-		}
-		if _, err := fmt.Fprintf(w, "%s: %s\n", e.k, e.v); err != nil {
-			return fmt.Errorf("writing %s: %w", e.k, err)
-		}
-	}
-	return nil
-}
